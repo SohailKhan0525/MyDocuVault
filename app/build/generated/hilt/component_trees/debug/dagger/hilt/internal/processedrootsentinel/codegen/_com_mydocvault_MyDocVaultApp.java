c2d8d9f4daebaeafec711c7a9379d725c26544a1
Mohd Zaheer Uddin <sohailkhannn.0525@gmail.com>
2026-02-14 09:36:27 +0000

    Remove build artifacts

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_mydocvault_MyDocVaultApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.mydocvault.MyDocVaultApp"
-)
-public final class _com_mydocvault_MyDocVaultApp {
-}
